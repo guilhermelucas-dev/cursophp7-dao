5a225e00c3d08d8e5c6b91b0d9caf76e9460267e
yellowskt <guilhermedesouzalucas@gmail.com>
2018-05-13 13:28:02 -0300

    metodos de ista

--- index.php
+++ index.php
@@ -9,10 +9,24 @@
         <?php
           require_once("config.php");
           
-          $root = new Usuario();
-          $root->loadById(4);
+          //Carrega um usuário  
+          //$root = new Usuario();
+          //$root->loadById(4);
+          //echo $root;
           
-          echo $root;
+          // Carrega uma lista de usuário 
+          // $lista = Usuario::getList();  
+          //echo json_encode($lista);
+          
+          //Carrega uma lista de usuários buscando pelo login
+          //$search = Usuario::search("jo");
+          //echo json_encode($search);
+          
+          //carrega um usuário usando o login e a senha
+          $usuario = new Usuario();
+          $usuario->login("Joao", "sfwr");
+          
+          echo $usuario;
         ?>
     </body>
 </html>
